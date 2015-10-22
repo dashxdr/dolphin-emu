4f8be5e881102cf00de2c9c9beb0d4dd26711ba4
Dave <dashxdr@gmail.com>
2015-10-22 10:29:42 -0500

    Moved info to DumpFrame.h file.

---
+++ Source/Core/VideoBackends/OGL/DumpFrame.h
@@ -0,0 +1,10 @@
+namespace OGL
+{
+extern int dumpframestate;
+extern int dumpframecount;
+extern FILE *dumpframefile;
+extern void write32(u32 v);
+extern void write4c(const char *s);
+extern void writepad(void);
+extern int dumpframeconstants;
+}
